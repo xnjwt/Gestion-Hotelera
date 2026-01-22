ee119931f26116446df1dc247b1e6dcd63184b0b
XtrangerN <xtrangernowrol@gmail.com>
2026-01-22 16:14:01 -0500

    Sí

--- Gestion Hotelera/Form1.Designer.cs
+++ Gestion Hotelera/Form1.Designer.cs
@@ -28,31 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(118, 74);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button2";
-            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Button button1;
     }
 }
