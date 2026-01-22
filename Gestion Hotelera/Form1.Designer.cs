894c0770b18c60306b95d95765e3d21dccf42204
XtrangerN <xtrangernowrol@gmail.com>
2026-01-22 16:07:01 -0500

    Ajustes en formulario principal

--- Gestion Hotelera/Form1.Designer.cs
+++ Gestion Hotelera/Form1.Designer.cs
@@ -37,7 +37,7 @@
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "button2";
             button1.UseVisualStyleBackColor = true;
             // 
             // Form1
