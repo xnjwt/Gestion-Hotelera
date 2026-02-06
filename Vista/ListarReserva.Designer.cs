fc14f8895cf19fbbe50f257b50046b9946e6f172
jaime <callejose642@gmail.com>
2026-02-05 22:42:04 -0500

    p

--- Vista/ListarReserva.Designer.cs
+++ Vista/ListarReserva.Designer.cs
@@ -30,14 +30,12 @@
         {
             label1 = new Label();
             dgvReserva = new DataGridView();
-            Nombres = new DataGridViewTextBoxColumn();
-            Correo = new DataGridViewTextBoxColumn();
-            Celular = new DataGridViewTextBoxColumn();
-            Rol = new DataGridViewTextBoxColumn();
-            Turno = new DataGridViewTextBoxColumn();
-            Estado = new DataGridViewTextBoxColumn();
             btnEliminar = new Button();
             btnActualizar = new Button();
+            Cliente = new DataGridViewTextBoxColumn();
+            Habitacion = new DataGridViewTextBoxColumn();
+            FechaEntrada = new DataGridViewTextBoxColumn();
+            FechaSalida = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dgvReserva).BeginInit();
             SuspendLayout();
             // 
@@ -45,7 +43,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(56, 114);
+            label1.Location = new Point(80, 133);
             label1.Name = "label1";
             label1.Size = new Size(118, 37);
             label1.TabIndex = 17;
@@ -56,68 +54,20 @@
             dgvReserva.BackgroundColor = Color.FromArgb(228, 191, 255);
             dgvReserva.BorderStyle = BorderStyle.Fixed3D;
             dgvReserva.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvReserva.Columns.AddRange(new DataGridViewColumn[] { Nombres, Correo, Celular, Rol, Turno, Estado });
-            dgvReserva.Location = new Point(56, 187);
+            dgvReserva.Columns.AddRange(new DataGridViewColumn[] { Cliente, Habitacion, FechaEntrada, FechaSalida });
+            dgvReserva.Location = new Point(96, 221);
             dgvReserva.Margin = new Padding(3, 4, 3, 4);
             dgvReserva.Name = "dgvReserva";
             dgvReserva.RowHeadersWidth = 51;
-            dgvReserva.Size = new Size(805, 283);
+            dgvReserva.Size = new Size(552, 283);
             dgvReserva.TabIndex = 14;
-            // 
-            // Nombres
-            // 
-            Nombres.DataPropertyName = "Nombres";
-            Nombres.HeaderText = "Nombres";
-            Nombres.MinimumWidth = 6;
-            Nombres.Name = "Nombres";
-            Nombres.Width = 200;
-            // 
-            // Correo
-            // 
-            Correo.DataPropertyName = "Email";
-            Correo.HeaderText = "Correo";
-            Correo.MinimumWidth = 6;
-            Correo.Name = "Correo";
-            Correo.Width = 200;
-            // 
-            // Celular
-            // 
-            Celular.DataPropertyName = "Celular";
-            Celular.HeaderText = "Celular";
-            Celular.MinimumWidth = 6;
-            Celular.Name = "Celular";
-            Celular.Width = 125;
-            // 
-            // Rol
-            // 
-            Rol.DataPropertyName = "Rol";
-            Rol.HeaderText = "Rol";
-            Rol.MinimumWidth = 6;
-            Rol.Name = "Rol";
-            Rol.Width = 125;
-            // 
-            // Turno
-            // 
-            Turno.DataPropertyName = "Turno";
-            Turno.HeaderText = "Turno";
-            Turno.MinimumWidth = 6;
-            Turno.Name = "Turno";
-            Turno.Width = 125;
-            // 
-            // Estado
-            // 
-            Estado.DataPropertyName = "Estado";
-            Estado.HeaderText = "Estado";
-            Estado.MinimumWidth = 6;
-            Estado.Name = "Estado";
-            Estado.Width = 125;
             // 
             // btnEliminar
             // 
             btnEliminar.BackColor = Color.MediumVioletRed;
             btnEliminar.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnEliminar.ForeColor = SystemColors.ButtonFace;
-            btnEliminar.Location = new Point(733, 114);
+            btnEliminar.Location = new Point(520, 131);
             btnEliminar.Margin = new Padding(3, 4, 3, 4);
             btnEliminar.Name = "btnEliminar";
             btnEliminar.Size = new Size(128, 51);
@@ -130,7 +80,7 @@
             btnActualizar.BackColor = Color.Indigo;
             btnActualizar.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnActualizar.ForeColor = SystemColors.ButtonFace;
-            btnActualizar.Location = new Point(598, 114);
+            btnActualizar.Location = new Point(385, 131);
             btnActualizar.Margin = new Padding(3, 4, 3, 4);
             btnActualizar.Name = "btnActualizar";
             btnActualizar.Size = new Size(128, 51);
@@ -138,11 +88,39 @@
             btnActualizar.Text = "Actualizar";
             btnActualizar.UseVisualStyleBackColor = false;
             // 
+            // Cliente
+            // 
+            Cliente.HeaderText = "Cliente";
+            Cliente.MinimumWidth = 6;
+            Cliente.Name = "Cliente";
+            Cliente.Width = 125;
+            // 
+            // Habitacion
+            // 
+            Habitacion.HeaderText = "Habitacion";
+            Habitacion.MinimumWidth = 6;
+            Habitacion.Name = "Habitacion";
+            Habitacion.Width = 125;
+            // 
+            // FechaEntrada
+            // 
+            FechaEntrada.HeaderText = "FechaEntrada";
+            FechaEntrada.MinimumWidth = 6;
+            FechaEntrada.Name = "FechaEntrada";
+            FechaEntrada.Width = 125;
+            // 
+            // FechaSalida
+            // 
+            FechaSalida.HeaderText = "FechaSalida";
+            FechaSalida.MinimumWidth = 6;
+            FechaSalida.Name = "FechaSalida";
+            FechaSalida.Width = 125;
+            // 
             // ListarReserva
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(956, 646);
+            ClientSize = new Size(744, 646);
             Controls.Add(btnEliminar);
             Controls.Add(btnActualizar);
             Controls.Add(label1);
@@ -158,13 +136,11 @@
 
         private Label label1;
         private DataGridView dgvReserva;
-        private DataGridViewTextBoxColumn Nombres;
-        private DataGridViewTextBoxColumn Correo;
-        private DataGridViewTextBoxColumn Celular;
-        private DataGridViewTextBoxColumn Rol;
-        private DataGridViewTextBoxColumn Turno;
-        private DataGridViewTextBoxColumn Estado;
         private Button btnEliminar;
         private Button btnActualizar;
+        private DataGridViewTextBoxColumn Cliente;
+        private DataGridViewTextBoxColumn Habitacion;
+        private DataGridViewTextBoxColumn FechaEntrada;
+        private DataGridViewTextBoxColumn FechaSalida;
     }
 }
